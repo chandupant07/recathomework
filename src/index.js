94fef257727c1a049964296c5e4b48bcc75c4e4a
chandupant07 <chandupant1111@gmail.com>
2023-06-29 21:48:45 +0530

    all commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+import { Hello } from './components/app';
+import { DataBindingComponent } from './components/DataBinding';
+import { DropDown } from './components/card';
+import { Databinddrop } from './components/integer';
+import { Datashow } from './components/dropdownlist';
+import { Tabledata } from './components/dataTable';
+import DataBindinAPI from './components/databindINAPI';
+import EventBinding from './components/eventBinding';
+import AddProgram from './components/addProgram';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <Hello /> */}
+    {/* <DataBindingComponent />
+    <DropDown />
+    <Databinddrop />
+    <Datashow />
+    <Tabledata />
+    <DataBindinAPI /> */}
+    {/* <EventBinding /> */}
+
+    <AddProgram />
+
   </React.StrictMode>
 );
 
